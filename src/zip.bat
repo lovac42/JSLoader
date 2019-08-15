7e02c6c85c907308daca5138e3b31afa55a05e0d
lovac42 <lovac42@protonmail.com>
2019-08-15 19:16:42 -0400

    added version to zipper

--- src/zip.bat
+++ src/zip.bat
@@ -1,13 +1,16 @@
 @echo off
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
 set REPO=jsloader
+set VERSION=0.0.2
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%/checksum.md5
 
 quick_manifest.exe "JS Loader" "%REPO%" >%REPO%/manifest.json
 
+echo %VERSION% >%REPO%/VERSION
+
 REM %ZIP% %REPO%_20.zip *.py %REPO%/*
 
 cd %REPO%
-%ZIP% ../%REPO%_21.ankiaddon *
+%ZIP% ../%REPO%_v%VERSION%_Anki21.ankiaddon *
